238c48106cb4fff3a51d2bc89ba6b08e07b1531c
Assa <assachana@gmail.com>
2015-11-26 12:57:29 +0000

    New features
    Added location, sunset, sunrise. no work done on battery

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,5 +1,7 @@
 var myAPIKey = 'c6a2cf9b29b37700c6eab49cf9d48b0a';
 
+var TESTPOSTURL = 'http://api.openweathermap.org/data/2.5/weather?lat=52.0373&lon=-0.7709310000000187&appid=c6a2cf9b29b37700c6eab49cf9d48b0a'
+
 var xhrRequest = function (url, type, callback) {
   var xhr = new XMLHttpRequest();
   xhr.onload = function () {
@@ -13,6 +15,10 @@ function locationSuccess(pos) {
   // Construct URL
   var url = "http://api.openweathermap.org/data/2.5/weather?lat=" +
       pos.coords.latitude + "&lon=" + pos.coords.longitude + '&appid=' + myAPIKey;
+  
+  //<><><><><><><><><><><>TESTING<><><><><><><><><><><><><>
+  //var url = TESTPOSTURL;
+  //
 
   // Send request to OpenWeatherMap
   xhrRequest(url, 'GET', 
@@ -32,11 +38,21 @@ function locationSuccess(pos) {
       var location = json.name;
       console.log("Location is " + location);
       
+      // Sunset
+      var sunset = convertUnixTimeStamp(json.sys.sunset);
+      console.log("Sunset is " + convertUnixTimeStamp(json.sys.sunset));
+      
+      // Sunrise
+      var sunrise = convertUnixTimeStamp(json.sys.sunrise);
+      console.log("Sunrise is " + convertUnixTimeStamp(json.sys.sunrise));
+      
       // Assemble dictionary using our keys
       var dictionary = {
         "KEY_TEMPERATURE": temperature,
         "KEY_CONDITIONS": conditions, 
-        "KEY_LOCATION": location
+        "KEY_LOCATION": location,
+        "KEY_SUNSET": sunset, 
+        "KEY_SUNRISE": sunrise
       };
 
       // Send to Pebble
@@ -50,6 +66,23 @@ function locationSuccess(pos) {
       );
     }      
   );
+}
+
+function convertUnixTimeStamp(timeStamp){
+  // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+  var date = new Date(timeStamp*1000);
+  // Hours part from the timestamp
+  var hours = date.getHours();
+  // Minutes part from the timestamp
+  var minutes = "0" + date.getMinutes();
+  // Seconds part from the timestamp
+  //var seconds = "0" + date.getSeconds();
+  
+  var ampm = (hours >= 12) ? "pm" : "am";
+
+  // Will display time in 10:30:23 format
+  var formattedTime = hours + ':' + minutes.substr(-2) + ampm; //+ ':' + seconds.substr(-2); dont really need the seconds
+  return formattedTime;
 }
 
 function locationError(err) {
--- src/main.c
+++ src/main.c
@@ -3,31 +3,48 @@
 #define KEY_TEMPERATURE 0
 #define KEY_CONDITIONS 1
 #define KEY_LOCATION 2
+#define KEY_SUNSET 3
+#define KEY_SUNRISE 4
 
 static Window *s_main_window;
 static TextLayer *s_time_layer, *s_date_layer;
 static TextLayer *s_weather_layer;
+static TextLayer *s_sun_layer;
+static int s_battery_level;
+static Layer *s_battery_layer;
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   // Store incoming information
   static char temperature_buffer[8];
   static char conditions_buffer[32];
   static char location_buffer[32];
+  static char sunset_buffer[9];
+  static char sunrise_buffer[9];
+  
+  static char sun_layer_buffer[32];
   static char weather_layer_buffer[32];
 
   // Read tuples for data
   Tuple *temp_tuple = dict_find(iterator, KEY_TEMPERATURE);
   Tuple *conditions_tuple = dict_find(iterator, KEY_CONDITIONS);
   Tuple *location_tuple = dict_find(iterator, KEY_LOCATION);
-
+  Tuple *sunset_tuple = dict_find(iterator, KEY_SUNSET);
+  Tuple *sunrise_tuple = dict_find(iterator, KEY_SUNRISE);
+  
   // If all data is available, use it
   if(temp_tuple && conditions_tuple) {
     snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)temp_tuple->value->int32);
     snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", conditions_tuple->value->cstring);
     snprintf(location_buffer, sizeof(location_buffer), "%s", location_tuple->value->cstring);
-
+    snprintf(sunset_buffer, sizeof(sunset_buffer), "%s", sunset_tuple->value->cstring);
+    snprintf(sunrise_buffer, sizeof(sunrise_buffer), "%s", sunrise_tuple->value->cstring);
+    
     // Assemble full string and display
-    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s, %s", temperature_buffer, conditions_buffer, location_buffer);
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s & %s in %s", temperature_buffer, conditions_buffer, location_buffer);
+    // Assemble sunset & sunrise string
+    snprintf(sun_layer_buffer, sizeof(sun_layer_buffer), "SR: %s || SS: %s", sunset_buffer, sunrise_buffer);
+    
+    text_layer_set_text(s_sun_layer, sun_layer_buffer);
     text_layer_set_text(s_weather_layer, weather_layer_buffer);
   }
 }
@@ -42,6 +59,13 @@ static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResul
 
 static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+}
+
+static void battery_callback(BatteryChargeState state) {
+  // Record the new battery level
+  s_battery_level = state.charge_percent;
+  // Update meter
+  layer_mark_dirty(s_battery_layer);
 }
 
 static void update_time() {
@@ -65,11 +89,26 @@ strftime(date_buffer, sizeof(date_buffer), "%a %d %b", tick_time);
   
 }
 
+static void battery_update_proc(Layer *layer, GContext *ctx) {
+  GRect bounds = layer_get_bounds(layer);
+
+  // Find the width of the bar
+  int width = (int)(float)(((float)s_battery_level / 100.0F) * 114.0F);
+
+  // Draw the background
+  graphics_context_set_fill_color(ctx, GColorBlack);
+  graphics_fill_rect(ctx, bounds, 0, GCornerNone);
+
+  // Draw the bar
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_fill_rect(ctx, GRect(0, PBL_IF_ROUND_ELSE(50, 50), bounds.size.w, 25), 0, GCornerNone);
+}
+
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
   update_time();
 
   // Get weather update every 30 minutes
-  if(tick_time->tm_min % 1 == 0) {
+  if(tick_time->tm_min % 15 == 0) {
     // Begin dictionary
     DictionaryIterator *iter;
     app_message_outbox_begin(&iter);
@@ -117,7 +156,7 @@ text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
   
   // Create temperature Layer
   s_weather_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(125, 130), bounds.size.w, 25));
+      GRect(0, PBL_IF_ROUND_ELSE(125, 130), bounds.size.w, 30));
 
   // Style the text
   text_layer_set_background_color(s_weather_layer, GColorWhite);
@@ -127,6 +166,25 @@ text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT ));
   
   layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_weather_layer));
+  
+  // Sun Layer 
+  s_sun_layer = text_layer_create(
+      GRect(0, PBL_IF_ROUND_ELSE(95, 95), bounds.size.w, 25));
+  text_layer_set_background_color(s_sun_layer, GColorWhite);
+  text_layer_set_text_color(s_sun_layer, GColorBlack);
+  text_layer_set_text_alignment(s_sun_layer, GTextAlignmentCenter);
+  text_layer_set_text(s_sun_layer, "<><><><><><>");
+  text_layer_set_font(s_sun_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14 ));
+  
+  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_sun_layer));
+  
+  // Create battery meter Layer
+s_battery_layer = layer_create(GRect(14, 54, 115, 2));
+layer_set_update_proc(s_battery_layer, battery_update_proc);
+  
+  // Add to Window
+layer_add_child(window_get_root_layer(window), s_battery_layer);
+  
 }
 
 static void main_window_unload(Window *window) {
@@ -138,6 +196,12 @@ static void main_window_unload(Window *window) {
   
   // Distroy Date layer 
   text_layer_destroy(s_date_layer);
+  
+  // Distory battery layer 
+  layer_destroy(s_battery_layer);
+  
+  // Distory sun layer 
+  text_layer_destroy(s_sun_layer);
 }
 
 static void init() {
@@ -161,6 +225,12 @@ static void init() {
 
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  
+  // Register for battery level updates
+  battery_state_service_subscribe(battery_callback);
+  
+  // Ensure battery level is displayed from the start
+  battery_callback(battery_state_service_peek());
   
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
